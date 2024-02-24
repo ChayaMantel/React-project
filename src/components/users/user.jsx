3a1f7145cd3956328513eb854fbae0a9187124b1
chayaMantel <chayam306@gmail.com>
2024-02-24 20:15:13 +0200

    change routes

--- src/components/users/user.jsx
+++ src/components/users/user.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import BusinessData from '../business/BusinessData';
 import ListService from '../servicelogic/listService';
 import AddMeeting from '../meeting/addMeeting';
@@ -6,11 +8,15 @@ import { Box, Button } from '@mui/material';
 export default function User() {
   return (
     <>
-
       <BusinessData />
       <AddMeeting />
       <ListService />
-
+      {/* Button to navigate to the login page */}
+      <div style={{ position: 'absolute', top: 0, left: 0, margin: '10px' }}>
+        <Button component={Link} to="/admin" variant="contained" color="secondary">
+          Go to Login
+        </Button>
+      </div>
     </>
   );
 }
